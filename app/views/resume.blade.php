4dc876e2cc55f1875d928ccea779e8f35b466383
Ben Bhamdeo <benjaminbhamdeo@gmail.com>
2013-06-12 23:54:25 -0400

    added thesis and some RA stuff

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -12,9 +12,10 @@
 					<div class="content-title-container">
 						<div class="resume-section-title"> Education </div>
 					</div>
+
 					<div class="resume-subject-title">Rutgers University 
 						<span class="bar-line">||</span> 
-						Sept. 2009 - May, 2013
+						Sept, 2009 - May, 2013
 					</div>
 					<div><em>Bachelor of Arts: </em>Classics w/ minors in History and Political Science</div> 
 					<div><em>GPA:</em> 3.919 <span class="bar-line">||</span> Summa Cum Laude</div>
@@ -30,7 +31,17 @@
 						<div class="resume-section-title"> Research & Writing </div>
 					</div>
 					<div> Honors Thesis <span class="bar-line">||</span> May, 2012 - May, 2013</div>
-					<div> Close reading Ancient Greek texts in Greek and translation</div>
+					<div> Classics: "Morality and Realpolitik in the Athenian Speech 
+						at the Peloponnesian Congress in Thucydides’ History of the Peloponnesian War "</div>
+					<div> Highest Honors</div>
+					<div> Close reading of Ancient Greek texts in Greek and translation. </div>
+					<div> Close reading of historical and linguistic commentary</div>
+					<div> Working closly with and Advisor </div>
+					<div> Understanding Greek language, politics, culture, warfare and diplomacy, philosophy, ethics, etc.</div>
+					<div> Presented research in 54 pg paper with oral defense</div>
+
+					<div> Research Assistant <span class="bar-line">||</span> Sept, 2012 - May, 2013</div>
+
 				</div>
 			</div>
 		</div>

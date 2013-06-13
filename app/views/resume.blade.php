52bc59d2b4ecd3b333f20bb08f09253057e73c2d
Ben Bhamdeo <benjaminbhamdeo@gmail.com>
2013-06-13 13:34:02 -0400

    added Education section to resume (general template) and css thereof

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -1,54 +1,69 @@
 @extends('layout')
 
 @section('resume-content')
+
 	<div class="title-box">
 		<div class="page-title"><span class="bar-line">|</span> Things I Do <span class="bar-line">|</span></div>
 	</div>
 	<div class="container-fluid">
 		<div class="row-fluid">
-				<div class="span3">
-	        <ul class="nav nav-list resume-sidenav" data-spy="affix" data-offset-top="100">
-	          <li class="active"><a href="#"><i class="icon-chevron-right"></i> Whales</a></li>
-	          <li class=""><a href="#"><i class="icon-chevron-right"></i> Whale penis</a></li>
-	          <li class=""><a href="#"><i class="icon-chevron-right"></i> Slam a Whale</a></li>
-	          <li class=""><a href="#"><i class="icon-chevron-right"></i> Get down With a Whale</a></li>
-	        </ul>
-	      </div>
-			<div class="span9">
-				<div class="content">
-					<div class="content-title-container">
-						<div class="resume-section-title"> Education </div>
+			<div class="span3">
+		        <ul class="nav nav-list resume-sidenav" data-spy="affix" data-offset-top="100">
+		          <li class="active"><a href="#education"><i class="icon-chevron-right"></i> Education</a></li>
+		          <li class=""><a href="#"><i class="icon-chevron-right"></i>Research</a></li>
+		          <li class=""><a href="#"><i class="icon-chevron-right"></i>Develop</a></li>
+		          <li class=""><a href="#"><i class="icon-chevron-right"></i>Teach</a></li>
+		          <li class=""><a href="#"><i class="icon-chevron-right"></i>Write</a></li>
+		        </ul>
+	      	</div>
+
+			<div class="span8">
+				<div class="resume-content-box"> 
+					<div class="education">
+						<div class="resume-title-container">
+							<div class="resume-section-title"> Education </div>
+						</div>
+
+						<div class="resume-subject-title">Rutgers University 
+							<span class="bar-line">||</span> 
+							Sept, 2009 - May, 2013
+						</div>
+						<ul class="unstyled">
+							<li>
+								<em>Bachelor of Arts: </em>Classics w/ minors in History and Political Science
+								<span class="bar-line">||</span> 
+								<em>GPA:</em> 3.919 
+								<span class="bar-line">||</span>
+								<em> Summa Cum Laude </em>
+							</li>
+						</ul> 
+						<div class="resume-subtitle"> Honors </div>
+							<ul class="unstyled">
+								<li>School of Arts and Sciences Honors Scholar</li>
+								<li>Greek and Latin Honors Scholar</li>
+								<li>Rutgers Robeson Scholar</li>
+								<li>Henry Rutgers Scholars Award</li>
+								<li>Outstanding Achievement in Undergraduate Research Award, 2012</li>
+								<li>Rutgers University Academic Excellence Award, 2011</li>
+							</ul>	
+
 					</div>
+					<div class="research">
+						<div class="content-title-container">
+							<div class="content-title"> Research & Writing </div>
+						</div>
+						<div> Honors Thesis <span class="bar-line">||</span> May, 2012 - May, 2013</div>
+						<div> Classics: "Morality and Realpolitik in the Athenian Speech 
+							at the Peloponnesian Congress in Thucydides’ History of the Peloponnesian War "</div>
+						<div> Highest Honors</div>
+						<div> Close reading of Ancient Greek texts in Greek and translation. </div>
+						<div> Close reading of historical and linguistic commentary</div>
+						<div> Working closly with and Advisor </div>
+						<div> Understanding Greek language, politics, culture, warfare and diplomacy, philosophy, ethics, etc.</div>
+						<div> Presented research in 54 pg paper with oral defense</div>
 
-					<div class="resume-subject-title">Rutgers University 
-						<span class="bar-line">||</span> 
-						Sept, 2009 - May, 2013
-					</div>
-					<div><em>Bachelor of Arts: </em>Classics w/ minors in History and Political Science</div> 
-					<div><em>GPA:</em> 3.919 <span class="bar-line">||</span> Summa Cum Laude</div>
-					<div> Honors </div>
-					<div>School of Arts and Sciences Honors Scholar</div>
-					<div>Greek and Latin Honors Scholar</div>
-					<div> Rutgers Robeson Scholar</div>
-					<div>Henry Rutgers Scholars Award</div>
-					<div>Outstanding Achievement in Undergraduate Research Award, 2012</div>
-					<div>Rutgers University Academic Excellence Award, 2011</div>
-
-					<div class="content-title-container">
-						<div class="resume-section-title"> Research & Writing </div>
-					</div>
-					<div> Honors Thesis <span class="bar-line">||</span> May, 2012 - May, 2013</div>
-					<div> Classics: "Morality and Realpolitik in the Athenian Speech 
-						at the Peloponnesian Congress in Thucydides’ History of the Peloponnesian War "</div>
-					<div> Highest Honors</div>
-					<div> Close reading of Ancient Greek texts in Greek and translation. </div>
-					<div> Close reading of historical and linguistic commentary</div>
-					<div> Working closly with and Advisor </div>
-					<div> Understanding Greek language, politics, culture, warfare and diplomacy, philosophy, ethics, etc.</div>
-					<div> Presented research in 54 pg paper with oral defense</div>
-
-					<div> Research Assistant <span class="bar-line">||</span> Sept, 2012 - May, 2013</div>
-
+						<div> Research Assistant <span class="bar-line">||</span> Sept, 2012 - May, 2013</div>
+					</div>		
 				</div>
 			</div>
 		</div>

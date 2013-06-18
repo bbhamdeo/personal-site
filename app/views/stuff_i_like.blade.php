0a9dfa81a9e20b8de80d5358800170882cf1086f
Ben Bhamdeo <benjaminbhamdeo@gmail.com>
2013-06-18 00:45:04 -0400

    added new content box backgrounds; filled in interests data, started interests page

--- app/views/stuff_i_like.blade.php
+++ app/views/stuff_i_like.blade.php
@@ -10,28 +10,90 @@
 
 		<div class="container-fluid">
 			<div class="row-fluid">
-				<div class="span12">	
+				<div class="span10 offset1">	
 					<div class="resume-content-box">
-						<p>I've always felt that a person's likes and 
+						<p id="interest-intro-text"  class="text-center">I've always felt that a person's likes and 
 							interests speaks volumes about them, even in ways that other more 
 							“conventional” types of explanation can’t. And so without further adieu! 
 						</p>
+
 						<div class="row-fluid">
+
+							<!-- Column1 -->
+							<div class="span5 offset1 tile"> 
+								<div> Editing Wikipedia </div>
+								<div> Fiction </div>
+								<div> Harry Potter </div>
+								<div> Star Wars</div>
+								<div> Avatar the Last Airbender (the tv show) </div>
+								<div> Writing </div>
+								<div> Valve </div>
+								<div> Coffee </div>
+								<div> World Culture </div>
+								<div> Technology </div>
+								<div> Animation </div>
+								<div> Source Filmmaker </div>
+								<div> Video Games </div>
+								<div> Art Direction </div>
+								<div> MMO's </div>
+								<div> Starcraft </div>
+								<div> The Old Republic </div>
+								<div> Broadway Musicals </div>
+								<div> Language </div>
+								<div> Health and Fitness </div>
+								<div> E-Sports </div>
+								<div> Stepen Colbert </div>
+								<div> All but one of my Rutgers Professors</div>
+								<div> Peter Jackson </div>
+								<div> Bioshock Infinite </div>
+								<div> KotOR </div>
+								<div> The Disney Renaissance </div>
+								<div>  </div>
+								<div>  </div>
+								<div>  </div>
+								<div>  </div>
+								<div>  </div>
+								<div>  </div>
+							</div>
+
+							<!-- Column 2 -->
+							<div class="span5 tile"> 
+								<div> Graphic Novels </div>
+								<div> Fantasy </div>
+								<div> J.R.R. Tolkien </div>
+								<div> Editing </div>
+								<div> Tea </div>
+								<div> Ancient History </div>
+								<div> Films and Filmmaking </div>
+								<div> Pixar </div>
+								<div> Level Design </div>
+								<div> World Building </div>
+								<div> LotRO </div>
+								<div> Pre-NGE SWG </div>
+								<div> Comedy </div>
+								<div> Drums </div>
+								<div> Meditation </div>
+								<div> Day[9] </div>
+								<div> Team Fortress 2</div>
+								<div> Law </div>
+								<div> Buisness Practices </div>
+								<div> Red Letter Media </div>
+								<div> The U.S. Supreme Court </div>		
+								<div> The WETA Workshop </div>
+								<div> That Game Company </div>
+								<div> Journey </div>	
+								<div> Howard Shore </div>
+								<div> J.K. Rowling </div>
+								<div> Web Design (duh :P) </div>		
+								<div>  </div>
+								<div>  </div>
+								<div>  </div>		
+							</div>
+						</div>
+<!-- 						<div class="row-fluid">
 							<div class="span6 tile"> TTEST</div>
 							<div class="span6 tile"> test </div>
-						</div>
-						<div class="row-fluid">
-							<div class="span6 tile"> TTEST</div>
-							<div class="span6 tile"> test </div>
-						</div>
-						<div class="row-fluid">
-							<div class="span6 tile"> TTEST</div>
-							<div class="span6 tile"> test </div>
-						</div>
-						<div class="row-fluid">
-							<div class="span6 tile"> TTEST</div>
-							<div class="span6 tile"> test </div>
-						</div>
+						</div> -->
 					</div>	
 				</div>
 			</div>

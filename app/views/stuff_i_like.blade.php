cb1fe04fdc62bf8283aa4dfbf33b90db85d9abe4
Ben Bhamdeo <benjaminbhamdeo@gmail.com>
2013-06-17 23:45:35 -0400

    started on things i like page

--- app/views/stuff_i_like.blade.php
+++ app/views/stuff_i_like.blade.php
@@ -3,10 +3,95 @@
 @section("stuff-i-like-content")
 
 	<body>
-		<div class="hero-unit">
-			<div> heerp </div>
+
+		<div class='title-box'>
+			 <div class="page-title"> Cool Things <span class="bar-line">&</span> Cool People</div>
 		</div>
+
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12">	
+					<div class="resume-content-box">
+						<p>I've always felt that a person's likes and 
+							interests speaks volumes about them, even in ways that other more 
+							“conventional” types of explanation can’t. And so without further adieu! 
+						</p>
+						<div class="row-fluid">
+							<div class="span6 tile"> TTEST</div>
+							<div class="span6 tile"> test </div>
+						</div>
+						<div class="row-fluid">
+							<div class="span6 tile"> TTEST</div>
+							<div class="span6 tile"> test </div>
+						</div>
+						<div class="row-fluid">
+							<div class="span6 tile"> TTEST</div>
+							<div class="span6 tile"> test </div>
+						</div>
+						<div class="row-fluid">
+							<div class="span6 tile"> TTEST</div>
+							<div class="span6 tile"> test </div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>	
 
 	</body>
 
 @stop
+
+
+<!-- 
+Things
+	Wikipedia
+	Comics
+	Fiction
+	Fantasy
+		Harry Potter
+		Lord of the Rings
+	Avatar the Last Airbender
+	Writing
+		Editing
+	World Culture
+	History
+	Ancient History
+	Tea
+	Coffee
+	Disney
+	Technology
+	Films
+		Filmmaking 
+		Animation
+			Pixar
+			Valve
+			Source Filmmaker
+	Video Games
+		Art Direction
+		World Building
+		MMO's
+			LOTRO
+			The Old Republic
+			Pre-NGE SWG
+	Foreign Language
+	Comedy
+	Music/Drums
+	health and fitness
+	E-Sports
+	Meditation 
+	Legal Studies
+	Buisness Practices 
+	Journey
+	Red Letter Media
+
+
+People 
+	Day9
+	Colbert
+	Justices
+	Rutgers
+	WETA
+	Peter Jackson
+	That Game Company
+
+-->
